8f00407c35e28ae8c2298cb97dba436ed9fa8c21
Evan Diewald <evandiewald@gmail.com>
2026-01-03 09:12:01 -0500

    adding custom domain

--- amplify/backend.ts
+++ amplify/backend.ts
@@ -1,8 +1,21 @@
-import { defineBackend } from '@aws-amplify/backend';
+import { defineBackend, secret } from '@aws-amplify/backend';
 import { auth } from './auth/resource';
 import { data } from './data/resource';
+import { Certificate } from 'aws-cdk-lib/aws-certificatemanager';
 
-defineBackend({
+const backend = defineBackend({
   auth,
   data,
+});
+
+const cfnDomain = backend.auth.resources.userPool
+
+cfnDomain.addDomain('customDomain', {
+  customDomain: {
+    domainName: 'auth.justchecklists.io',
+    certificate: Certificate.fromCertificateArn(
+      cfnDomain, 'certificate',
+      'arn:aws:acm:us-east-1:668146110194:certificate/c2d8a762-a484-4059-843d-bc5d8400c7d1'
+    )
+  }
 });
